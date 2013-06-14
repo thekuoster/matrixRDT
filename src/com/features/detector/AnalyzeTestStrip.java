7dd4e4642a5cb842aa31306363609615caf41d97
Emily Kuo <ekuo8@mit.edu>
2013-06-14 15:44:45 -0400

    added commenting to the files

--- src/com/features/detector/AnalyzeTestStrip.java
+++ src/com/features/detector/AnalyzeTestStrip.java
@@ -37,6 +37,16 @@ public class AnalyzeTestStrip extends AsyncTask<Bitmap, Integer, Mat>{
 	private final int[] positive = {1,1,0,0};
 	private final int[] negative = {1,0,0,0};
 	
+	// Look at http://docs.opencv.org/doc/tutorials/imgproc/imgtrans/canny_detector/canny_detector.html
+	// for more information on choosing threshold values
+	private double cannyThresh = 95;
+	private double cannyRatio = .4;
+	
+	// Look at http://docs.opencv.org/doc/tutorials/imgproc/imgtrans/hough_lines/hough_lines.html
+	// for more information on choosing threshold values
+	private int houghThresh = 120;
+	
+	
 	public AnalyzeTestStrip(Activity context){
 		this.context = context;
 	}
@@ -44,7 +54,7 @@ public class AnalyzeTestStrip extends AsyncTask<Bitmap, Integer, Mat>{
 	private int imgWidth;
 	private int imgHeight;
 	
-	private int threshold = 105;
+	private int grayThresh = 105;
 	
 	private int[][] markers = new int[4][8];
 	
@@ -64,15 +74,15 @@ public class AnalyzeTestStrip extends AsyncTask<Bitmap, Integer, Mat>{
 		imgWidth = imgInput.width();
 		imgHeight = imgInput.height();
 		
-		//Imgproc.cvtColor(imgInput, imgOutput, Imgproc.COLOR_RGB2HSV);
+		// create a grayscale image
 		Imgproc.cvtColor(imgInput, imgGray, Imgproc.COLOR_RGB2GRAY);
 		
 		Mat imgContours = new Mat();
-		double thresh = 95;
-		Imgproc.Canny(imgGray, imgContours, .4*thresh, thresh);
+		Imgproc.Canny(imgGray, imgContours, cannyRatio*cannyThresh, cannyThresh);
 		
+		// Output for the hough transform
 		MatOfPoint2f lines = new MatOfPoint2f();
-		Imgproc.HoughLines(imgContours, lines, 1, Math.PI/180, 120);
+		Imgproc.HoughLines(imgContours, lines, 1, Math.PI/180, houghThresh);
 		
 		
 		//Imgproc.cvtColor(imgInput, imgOutput, Imgproc.COLOR_GRAY2RGB);
@@ -82,7 +92,7 @@ public class AnalyzeTestStrip extends AsyncTask<Bitmap, Integer, Mat>{
 		
 		ArrayList<Line> lnList = new ArrayList<Line>();
 		
-		// iterate over found lines
+		// iterate over found lines, converts from polar to cartesian coordinates
 		for (int i = 0; i < lnsArray.length; i++){
 			double theta = lnsArray[i].y;
 			double rho = lnsArray[i].x;
@@ -96,9 +106,12 @@ public class AnalyzeTestStrip extends AsyncTask<Bitmap, Integer, Mat>{
 			//there should be 4 lines
 			lnList.add(new Line(pt1, pt2));
 			
-			//Core.line(imgOutput, pt1, pt2, new Scalar(255, 0, 0) , 1);
+			
+			// Draws result from hough transform
+			//Core.line(imgOutput, pt1, pt2, new Scalar(255, 0, 0) , 1); (red lines)
 		}
 		
+		/* Temporary statement, cancels task if the number of lines isn't equal to 4*/
 		if(lnList.size() != 4){
 			Log.w("Debug", "cancelled task");
 			this.cancel(true);
@@ -109,17 +122,19 @@ public class AnalyzeTestStrip extends AsyncTask<Bitmap, Integer, Mat>{
 			Point[] corners = getIntersections(lnList);
 			corners = sortCorners(corners);
 			
-			/*//draw the test area
+			/*//draw the test area (blue lines)
 			int numCorners = corners.length;
 			for(int i = 0; i < numCorners; i++){
 				Core.line(imgOutput, corners[i], corners[(i+1)%numCorners], new Scalar(0,0,255), 2);
 			}*/
 			
 			//scanTestArea(corners, imgOutput);
-			List<Rect> pos = scanTestArea(corners, imgGray);
+			
+			List<Rect> pos = scanTestArea(corners, imgGray); // currently scans based on grayscale values
 			
 			Imgproc.cvtColor(imgInput, imgInput, Imgproc.COLOR_RGBA2RGB);
 			
+			// draw identified positive markers on the test strip
 			for(Rect rect : pos){
 				Point pt1 = new Point(rect.x, rect.y);
 				Point pt2 = new Point(rect.x+rect.width, rect.y+rect.height);
@@ -127,6 +142,7 @@ public class AnalyzeTestStrip extends AsyncTask<Bitmap, Integer, Mat>{
 				Core.rectangle(imgInput, pt1, pt2, new Scalar(0, 188, 00), -1);
 			}
 			
+			// copies just the test area to a new image
 			Mat testArea = imgInput.submat((int)corners[0].y, (int)corners[3].y, (int)corners[0].x, (int)corners[1].x);
 			
 			return testArea;
@@ -139,10 +155,11 @@ public class AnalyzeTestStrip extends AsyncTask<Bitmap, Integer, Mat>{
 		if(result != null){
 			super.onPostExecute(result);
 			
+			// change view to results page
 			context.setContentView(R.layout.results_page);
 			context.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 			
-			
+			// get columns of matrix
 			for (int i = 0; i < tests.length; i++) {
 				int[] res = new int[markers.length];
 				for (int j = 0; j < markers.length; j++) {
@@ -173,11 +190,19 @@ public class AnalyzeTestStrip extends AsyncTask<Bitmap, Integer, Mat>{
 		Toast error = Toast.makeText(context, text, duration);
 		error.show();
 		
+		// go back to the beginning on error
 		context.setContentView(R.layout.home_page);
 		context.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 		
 	}
 	
+	/**
+	 * Finds all the intersection points of a given set of lines. The relevant intersection points
+	 * are those found to be within the bounds of the image being analyzed
+	 * 
+	 * @param lnList
+	 * @return an array of the intersection points
+	 */
 	private Point[] getIntersections(ArrayList<Line> lnList){
 		Set<Point> intersections = new HashSet<Point>();
 		
@@ -185,6 +210,7 @@ public class AnalyzeTestStrip extends AsyncTask<Bitmap, Integer, Mat>{
 			for(int j = i+1; j < lnList.size(); j++){
 				Point is = getIntersection(lnList.get(i), lnList.get(j));
 				if(is != null){
+					// checks whether the intersection point is within the image
 					if(is.x >= 0 && is.x < imgWidth && is.y >= 0 && is.y < imgHeight){
 						intersections.add(is);
 					}
@@ -221,30 +247,104 @@ public class AnalyzeTestStrip extends AsyncTask<Bitmap, Integer, Mat>{
 		return is;
 		
 	}
-	
-	protected class Line{
-		Point pt1;
-		Point pt2;
 		
-		protected Line(Point p1, Point p2){
-			pt1 = p1;
-			pt2 = p2;
-		}
-	}
-	
 	/**
-	 * Obtains the delta between two points
+	 * Performs the pixel analysis of the the test area of the image
 	 * 
-	 * @param p1
-	 * @param p2
+	 * @param corners the list of corners starting from the top left and going clockwise of the rectangle enclosing the test area
+	 * @param output the 
 	 * @return
 	 */
-	private Point delta(Point p1, Point p2){
-		return new Point(p1.x-p2.x, p1.y-p2.y);
+	private List<Rect> scanTestArea(Point[] corners, Mat output){
+		List <Rect> positives = new ArrayList<Rect>();
+		
+		double height = Math.round(lnLength(new Line(corners[0], corners[3])));
+		double width = Math.round(lnLength(new Line(corners[0], corners[1])));
+		
+		/*Log.w("Debug", corners[0].toString());
+		Log.w("Debug", corners[1].toString());
+		Log.w("Debug", corners[2].toString());
+		Log.w("Debug", corners[3].toString());*/
+		
+		double secWidth = width/19;
+		double secHeight = height/5;
+		
+		int scanStart = (int) (corners[0].y+secHeight/2);
+		int scanEnd = (int) (scanStart + 4*secHeight);
+		
+		Range first = new Range();
+		Range second = new Range();
+		Range third = new Range();
+		Range fourth = new Range();
+		
+		Range[] sections = {first, second, third, fourth};
+		
+		// set the Y ranges that encompass each of the markers
+		for(int i=0; i<=4; i++){
+			Point pt1 = new Point(corners[0].x, corners[0].y + secHeight * (i + .5));
+			//Point pt2 = new Point(pt1.x + width, pt1.y);
+			
+			if(i != 4){
+				sections[i].start = (int)pt1.y;
+			}
+			
+			if(i != 0){
+				sections[i-1].end = (int)pt1.y;
+			}
+			//Core.line(output, pt1, pt2, new Scalar(0, 255, 0));
+		}
+		
+		// keeps track of which test strip we are on
+		int counter = 0;
+		
+		// controls scanning horizontally
+		for(int i = 2; i <= 16; i+=2){
+			//Point pt1 = new Point(corners[0].x + secWidth * (i+.5), corners[0].y);
+			//Point pt2 = new Point(pt1.x, pt1.y+height);
+			//Core.line(output, pt1, pt2, new Scalar(0, 255, 0));
+			
+			int x = (int) (corners[0].x+secWidth * (i+.5));
+			
+			// controls the vertical scan
+			for (int j = scanStart; j <= scanEnd; j++){
+				int start = -1;
+				int end = -1;
+				double[] vals = output.get(j, x);
+			
+				if(vals != null && vals[0] < grayThresh){
+					
+					// finds areas less than the gray threshold value
+					start = j;
+					while(j <= scanEnd && output.get(j, x)[0] < grayThresh){
+						j++;
+					}
+					end = j;
+					
+					// gets the center of identified section section
+					int middle = (start + end)/2;
+					
+					// small rectangle area inside the identified region
+					Rect m = new Rect(x-2, middle-2, 3, 3); // 3 is arbitrary for this image. should probably be proportional to test are size
+					
+					// gets the average grayscale for the rectangular region
+					if(getAverage(m, output) < grayThresh){
+						int row = getSection(sections, middle);
+						if(row >= 0){
+							markers[row][counter]=1;
+						}
+						positives.add(m);
+					}
+					
+				}
+				
+			}
+			counter++;
+		}
+		return positives;
+		
 	}
 	
-	/*assumes there are only 4 corners
-	 corners are sorted from topLeft going clockwise*/
+	
 	/**
 	 * Sorts the corners in order from the topLeft going clockwise. Assumes there are only 4 corners to sort
 	 * 
@@ -257,6 +357,7 @@ public class AnalyzeTestStrip extends AsyncTask<Bitmap, Integer, Mat>{
 		Line maxSide = null;
 		Point other = null;
 		
+		// Takes the first three intersections and finds the hypotenuse of the triangle to order the points
 		for(int i = 0; i < 3; i++){
 			for(int j = i+1; j < 3; j++){
 				Line ln = new Line(corners[i], corners[j]);
@@ -269,6 +370,7 @@ public class AnalyzeTestStrip extends AsyncTask<Bitmap, Integer, Mat>{
 			}
 		}
 		
+		// corners are ordered, but not necessarily with top left first and going clockwise
 		Point[] halfSorted = {maxSide.pt1, other, maxSide.pt2, corners[3]};
 		
 		double minX1 = -1;
@@ -304,6 +406,7 @@ public class AnalyzeTestStrip extends AsyncTask<Bitmap, Integer, Mat>{
 		int topLeft;
 		int direction;
 		
+		// find which index is the top left point and get whether the points are ordered clockwise or counter-clockwise
 		if(min1.y < min2.y){
 			topLeft = index1;
 			direction = getDirection(index1, index2, numCorners);
@@ -314,6 +417,7 @@ public class AnalyzeTestStrip extends AsyncTask<Bitmap, Integer, Mat>{
 		}
 		
 		
+		// resorts the corners
 		Point[] sorted = new Point[numCorners];
 		for(int i = 0; i < numCorners; i++){
 			int index = (topLeft+(i*direction) + numCorners) % numCorners;
@@ -326,8 +430,8 @@ public class AnalyzeTestStrip extends AsyncTask<Bitmap, Integer, Mat>{
 	/**
 	 * Gets which direction the corners are currently ordered in
 	 * 
-	 * @param first index of the first point
-	 * @param second index of the second point
+	 * @param first index of the top left point
+	 * @param second index of next point
 	 * @param num 
 	 * @return
 	 */
@@ -352,83 +456,15 @@ public class AnalyzeTestStrip extends AsyncTask<Bitmap, Integer, Mat>{
 		return Math.sqrt(Math.pow(p1.x-p2.x,2) + Math.pow(p1.y-p2.y,2));
 	}
 	
-	private List<Rect> scanTestArea(Point[] corners, Mat output){
-		List <Rect> positives = new ArrayList<Rect>();
-		
-		double height = Math.round(lnLength(new Line(corners[0], corners[3])));
-		double width = Math.round(lnLength(new Line(corners[0], corners[1])));
-		
-		/*Log.w("Debug", corners[0].toString());
-		Log.w("Debug", corners[1].toString());
-		Log.w("Debug", corners[2].toString());
-		Log.w("Debug", corners[3].toString());*/
-		
-		double secWidth = width/19;
-		double secHeight = height/5;
-		
-		int scanStart = (int) (corners[0].y+secHeight/2);
-		int scanEnd = (int) (scanStart + 4*secHeight);
-		
-		Range first = new Range();
-		Range second = new Range();
-		Range third = new Range();
-		Range fourth = new Range();
-		
-		Range[] sections = {first, second, third, fourth};
-		
-		for(int i=0; i<=4; i++){
-			Point pt1 = new Point(corners[0].x, corners[0].y + secHeight * (i + .5));
-			//Point pt2 = new Point(pt1.x + width, pt1.y);
-			
-			if(i != 4){
-				sections[i].start = (int)pt1.y;
-			}
-			
-			if(i != 0){
-				sections[i-1].end = (int)pt1.y;
-			}
-			//Core.line(output, pt1, pt2, new Scalar(0, 255, 0));
-		}
-		int counter = 0;
-		
-		for(int i = 2; i <= 16; i+=2){
-			//Point pt1 = new Point(corners[0].x + secWidth * (i+.5), corners[0].y);
-			//Point pt2 = new Point(pt1.x, pt1.y+height);
-			//Core.line(output, pt1, pt2, new Scalar(0, 255, 0));
-			
-			int x = (int) (corners[0].x+secWidth * (i+.5));
-			
-			for (int j = scanStart; j <= scanEnd; j++){
-				int start = -1;
-				int end = -1;
-				double[] vals = output.get(j, x);
-			
-				if(vals != null && vals[0] < threshold){
-					
-					start = j;
-					while(j <= scanEnd && output.get(j, x)[0] < threshold){
-						j++;
-					}
-					end = j;
-					
-					int middle = (start + end)/2;
-					
-					Rect m = new Rect(x-2, middle-2, 3, 3);
-					if(getAverage(m, output) < threshold){
-						int row = getSection(sections, middle);
-						if(row >= 0){
-							markers[row][counter]=1;
-						}
-						positives.add(m);
-					}
-					
-				}
-				
-			}
-			counter++;
-		}
-		return positives;
-		
+	/**
+	 * Obtains the delta between two points
+	 * 
+	 * @param p1
+	 * @param p2
+	 * @return
+	 */
+	private Point delta(Point p1, Point p2){
+		return new Point(p1.x-p2.x, p1.y-p2.y);
 	}
 	
 	private int getAverage(Rect rect, Mat gray){
@@ -444,6 +480,13 @@ public class AnalyzeTestStrip extends AsyncTask<Bitmap, Integer, Mat>{
 		return sum/(rect.width*rect.height);
 	}
 	
+	/**
+	 * Determines which marker within each test is being analyzed
+	 * 
+	 * @param sections
+	 * @param center
+	 * @return
+	 */
 	private int getSection(Range[] sections, int center){
 		for (int i = 0; i < sections.length-1; i++) {
 			Range sect = sections[i];
@@ -454,6 +497,12 @@ public class AnalyzeTestStrip extends AsyncTask<Bitmap, Integer, Mat>{
 		return -1;
 	}
 	
+	/**
+	 * Returns the result of the test given the contents of each test column
+	 * 
+	 * @param test
+	 * @return
+	 */
 	private int getResult(int[] test){
 		if(Arrays.equals(test, positive)){
 			return R.string.positive_result;
@@ -464,4 +513,13 @@ public class AnalyzeTestStrip extends AsyncTask<Bitmap, Integer, Mat>{
 		return R.string.error_result;
 	}
 
+	protected class Line{
+		Point pt1;
+		Point pt2;
+		
+		protected Line(Point p1, Point p2){
+			pt1 = p1;
+			pt2 = p2;
+		}
+	}
 }
